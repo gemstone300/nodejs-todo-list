6df9ac34b3a805df49902e2c6d00c6d2ce33b197
gemstone300 <claire.gemstone300@gmail.com>
2024-01-22 22:15:20 +0900

    first commit

---
+++ joi-example.js
@@ -0,0 +1,34 @@
+import Joi from "joi";
+
+// Joi 스키마를 정의합니다.
+const schema = Joi.object({
+  // name Key는 문자열 타입이고, 필수로 존재해야합니다.
+  // 문자열은 최소 3글자, 최대 30글자로 정의합니다.
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+});
+
+// 검증할 데이터를 정의합니다.
+const user = { name: "Fd", email: "foo@example.com" };
+
+// schema를 이용해 user 데이터를 검증합니다.
+const validation = schema.validate(user);
+console.log(validation);
+
+// 검증 결과값 중 error가 존재한다면 에러 메시지를 출력합니다.
+// if (validation.error) {
+//   console.log(validation.error.message);
+// } else {
+//   // 검증 결과값 중 error가 존재하지 않는다면, 데이터가 유효하다는 메시지를 출력합니다.
+//   console.log("Valid Data!");
+// }
+
+// try {
+//   // schema를 이용해 user 데이터를 검증합니다.
+//   const validation = await schema.validateAsync(user);
+//   // 검증 결과값 중 error가 존재하지 않는다면, 데이터가 유효하다는 메시지를 출력합니다.
+//   console.log("Valid Data!");
+// } catch (error) {
+//   // 검증에 실패한다면, 에러 메시지를 출력합니다.
+//   console.log(error.message);
+// }
---
+++ routes/todos.router.js
@@ -0,0 +1,119 @@
+// /routes/todos.router.js
+
+import express from "express";
+import Todo from "../schemas/todo.schema.js";
+import Joi from "joi";
+
+const router = express.Router();
+
+// 할 일 생성 API의 요청 데이터 검증을 위한 Joi 스키마를 정의합니다.
+const createTodoSchema = Joi.object({
+  value: Joi.string().min(1).max(50).required(),
+});
+
+router.post("/todos", async (req, res, next) => {
+  try {
+    // 클라이언트에게 전달받은 데이터를 검증합니다.
+    const validateBody = await createTodoSchema.validateAsync(req.body);
+
+    // 클라이언트에게 전달받은 value 데이터를 변수에 저장합니다.
+    //const { value } = req.body;
+    const { value } = validateBody;
+
+    // // value가 존재하지 않을 때, 클라이언트에게 에러 메시지를 전달합니다.
+    // if (!value) {
+    //   return res
+    //     .status(400)
+    //     .json({ errorMessage: "해야할 일 데이터가 비어있습니다." });
+    // }
+
+    // Todo모델을 사용해, MongoDB에서 'order' 값이 가장 높은 '해야할 일'을 찾습니다.
+    const todoMaxOrder = await Todo.findOne().sort("-order").exec();
+
+    // 'order' 값이 가장 높은 도큐멘트의 1을 추가하거나 없다면, 1을 할당합니다.
+    const order = todoMaxOrder ? todoMaxOrder.order + 1 : 1;
+
+    // Todo모델을 이용해, 새로운 '해야할 일'을 생성합니다.
+    const todo = new Todo({ value, order });
+
+    // 생성한 '해야할 일'을 MongoDB에 저장합니다.
+    await todo.save();
+
+    return res.status(201).json({ todo });
+  } catch (error) {
+    next(error);
+    //   console.error(error);
+    //   // Joi 검증에서 에러가 발생하면, 클라이언트에게 에러 메시지를 전달합니다.
+    //   if (error.name === "ValidationError") {
+    //     return res.status(400).json({ errorMessage: error.message });
+    //   }
+
+    //   // 그 외의 에러가 발생하면, 서버 에러로 처리합니다.
+    //   return res
+    //     .status(500)
+    //     .json({ errorMessage: "서버에서 에러가 발생하였습니다." });
+  }
+});
+
+router.get("/todos", async (req, res, next) => {
+  // Todo모델을 이용해, MongoDB에서 'order' 값이 가장 높은 '해야할 일'을 찾습니다.
+  const todos = await Todo.find().sort("-order").exec();
+
+  // 찾은 '해야할 일'을 클라이언트에게 전달합니다.
+  return res.status(200).json({ todos });
+});
+
+router.patch("/todos/:todoId", async (req, res, next) => {
+  // 변경할 '해야할 일'의 ID 값을 가져옵니다.
+  const { todoId } = req.params;
+  // '해야할 일'을 몇번째 순서로 설정할 지 order 값을 가져옵니다.
+  const { order, done, value } = req.body;
+  // 변경하려는 '해야할 일'을 가져옵니다. 만약, 해당 ID값을 가진 '해야할 일'이 없다면 에러를 발생시키기
+  const currentTodo = await Todo.findById(todoId).exec();
+  if (!currentTodo) {
+    return res
+      .status(404)
+      .json({ errorMessage: "존재하지 않는 todo 데이터입니다." });
+  }
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      // 해당 order 값을 가진 '해야할 일'이 있다면, 해당 '해야할 일'의 order 값을 변경하고 저장
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    // 변경하려는 '해야할 일'의 order 값을 변경
+    currentTodo.order = order;
+  }
+  if (value) {
+    currentTodo.value = value;
+  }
+  if (done !== undefined) {
+    // 변경하려는 '해야할 일'의 doneAt 값을 변경합니다.
+    currentTodo.doneAt = done ? new Date() : null;
+  }
+
+  // 변경된 '해야할 일'을 저장
+  await currentTodo.save();
+
+  return res.status(200).json({});
+});
+
+router.delete("/todos/:todoId", async (req, res) => {
+  // 삭제할 '해야할 일'의 ID 값을 가져옵니다.
+  const { todoId } = req.params;
+
+  // 삭제하려는 '해야할 일'을 가져옵니다. 만약, 해당 ID값을 가진 '해야할 일'이 없다면 에러를 발생시킵니다.
+  const todo = await Todo.findById(todoId).exec();
+  if (!todo) {
+    return res
+      .status(404)
+      .json({ errorMessage: "존재하지 않는 todo 데이터입니다." });
+  }
+
+  // 조회된 '해야할 일'을 삭제합니다.
+  await Todo.deleteOne({ _id: todoId }).exec();
+
+  return res.status(200).json({});
+});
+export default router;
